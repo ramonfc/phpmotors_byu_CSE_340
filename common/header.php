8e50677bceda5e7c8f6199c52a5df9b968ecd36c
ramon <monchouam@gmail.com>
2023-11-08 20:40:38 -0500

    se cambia el title de logout

--- common/header.php
+++ common/header.php
@@ -7,7 +7,7 @@
   if (isset($_SESSION['loggedin'])) {
     echo "<a href='/phpmotors/accounts/?action=admin' class='header_link'>Welcome " . $_SESSION['clientData']['clientFirstname'] . "</a>";
 
-    echo  "<a href='/phpmotors/accounts/?action=Logout' title='Logout' class='header_link header_link_right_space'>Logout</a>";
+    echo  "<a href='/phpmotors/accounts/?action=Logout' title='logout session' class='header_link header_link_right_space'>Logout</a>";
   } else {
     echo "<a href='/phpmotors/accounts/?action=deliverLoginView' title='my account' class='header_link header_link_right_space'>My Account</a>";
   }
